19dbbf6a0a90d781ecc23d9c10888829e665c01e
Ji-Yuhang <yuhang.silence@gmail.com>
2016-05-06 17:16:26 +0800

    Refactor tests to user helpe methods

--- spec/features/user_creates_todo_spec.rb
+++ spec/features/user_creates_todo_spec.rb
@@ -4,11 +4,9 @@ feature "Use creates todo" do
   scenario "successfully" do
     sign_in
     #visit root_path
-    click_on "Add a new todo"
-    fill_in "Title", with: "Buy milk"
-    click_on "Submit"
+    create_todo "Buy milk"
 
-    expect(page).to have_css '.todos li', text: "Buy milk"
+    expect(page).to display_todo "Buy milk"
     #post create_todo_path
   end
 end
--- spec/features/user_sees_own_todos_spec.rb
+++ spec/features/user_sees_own_todos_spec.rb
@@ -4,6 +4,6 @@ feature "User sess own todos" do
   scenario "doesn't see other's todos" do
     Todo.create!(title: "Buy milk", email:"yuhang.silence_else@gmail.com")
     sign_in_as "yuhang.silence@gmail.com"
-    expect(page).not_to have_css ".todo li", text: "Buy milk"
+    expect(page).not_to display_todo "Buy milk"
   end
 end
