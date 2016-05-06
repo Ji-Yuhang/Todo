b6035f73ccab05344415dbb29c44358b088c3353
Ji-Yuhang <yuhang.silence@gmail.com>
2016-05-06 17:40:53 +0800

    Add mark incomplete

--- app/controllers/completions_controller.rb
+++ app/controllers/completions_controller.rb
@@ -1,6 +1,14 @@
 class CompletionsController < ApplicationController
   def create
-    todo.touch :completed_at
+    #todo.touch :completed_at
+    todo.complete!
+    redirect_to todos_path
+
+  end
+
+  def destroy
+    todo.mark_incomplete!
+    todo.update_column :completed_at, nil
     redirect_to todos_path
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "todos#index"
   resources :todos, only: [:new,:create, :index] do
-    resource :completion, only: :create
+    resource :completion, only: [:create, :destroy]
   end
   #resources :sessions, only: [:new,:create]
   resource :session, only: [:new,:create]
